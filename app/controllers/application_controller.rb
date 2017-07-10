94bf629e3e89153aad92bdb882ce91caeeecce5f
yu_honda <yhonda@mikilab.doshisha.ac.jp>
2017-07-11 00:38:26 +0900

    【追加】挨拶の表示機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html:"Hello World!"
+  end
+
+  def goodbye
+    render html:"Good Bye!"
+  end
+  
 end
